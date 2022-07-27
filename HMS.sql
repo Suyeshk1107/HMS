bae8b6fa2a532403ea6924f46e563ac13816474f
Suyesh Kumar <71451476+Suyeshk1107@users.noreply.github.com>
2022-07-27 22:52:54 +0530

    Dummy data for hms

---
+++ HMS.sql
@@ -0,0 +1,83 @@
+create database Hospital;
+use Hospital;
+
+-- tables
+
+create table doctor(
+doctor_id int auto_increment primary key,
+name_of_doctor varchar(25) not null,
+specialisation varchar(30) not null,
+experience int not null,
+gender char(1) not null,
+age int not null,
+Contact_number varchar(15) not null unique,
+Address varchar(100)
+);
+
+create table patient(
+patient_id int auto_increment primary key,
+name_of_patient varchar(25) not null,
+gender char(1) not null,
+age int not null,
+Contact_number varchar(15) not null,
+Address varchar(50),
+department varchar(15)
+);
+
+create table login_credentials(
+id int not null,
+user_name varchar(15),
+password varchar(15)
+);
+
+create table regular_schedule(
+doctor_id int not null,
+name_of_doctor varchar(25) not null,
+available_day varchar(10) not null,
+slot_start time not null,
+slot_end time not null,
+constraint fk_docId foreign key(doctor_id)
+references doctor(doctor_id)
+);
+
+
+create table appointments(
+patient_id int not null,
+name_of_patient varchar(25) not null,
+slot time not null,
+doctor_id int auto_increment primary key,
+name_of_doctor varchar(25) not null
+);
+
+
+-- data
+
+insert ignore into doctor(name_of_doctor, specialisation, experience, gender, age, contact_number, address)
+values('Dr Pullen', 'Cardiologists', 5, 'm', 30 , '+1 582-558-3861', '1128, 3rd Floor, Fat No 13a Wings, Sanjay Society, Prabhadevi'),
+('Dr Fillmore', 'Anesthesiologists', 15, 'f', 50 , '+1 464-485-6722', '91, Shri Krishna Bhavan, Hindu Colony, Dadar(e)'),
+('Dr Mangle', 'Dermatologists', 10, 'm', 45 , '+1 312-795-2062', ' 20/1, North Crescent Road, T Nagar'),
+('Dr Ken Hurt', 'Neurologists', 8, 'm', 38 , '+1 319-730-5064', '18th Road, Opp Electric Office, Chembur (east)'),
+('Dr B. Sick', 'Gastroenterologists', 3, 'f', 34 , '+1 218-654-7743', 'Central Lane, Bengali Market'),
+('Dr Watamaniuk', 'Dermatologists', 11, 'm', 45 , '+1 202-411-8219', 'S 614, Nehru Enclave, School Block, Shakarpur'),
+('Dr Lipp', 'Cardiologists', 25, 'f', 55 , '+1 305-270-1658', 'A-11, Part 2, Greater Kailash Enclave'),
+('Dr Carey', 'Dermatologists', 5, 'f', 40 , '+1 225-897-3950', '106, Sarita Bldg, Near Dahisar Tol Naka, Dahisar'),
+('Dr Nervo', 'Neurologists', 11, 'f', 47 , '+1 582-262-9538', 'Opp S B I,dr V S Road, Ambawadi'),
+('Dr Hurt', 'Dermatologists', 10, 'm', 50 , '+1 434-723-6523', ' Samta Nagar Tel Exchange, Western Express Highway, Nr Santa');
+
+insert ignore into patient(name_of_patient, gender, age, contact_number, address, department)
+values('Bhuwnesh', 'm', 24, '9876987655', '11, Welcome Shpg Centre, Old Padra Road, Old Padra Road','Gastroenterologists'),
+('Harsha','m', 23, '348628643',' G-3 Anmol, Ground Floor, Grant Road','Anesthesiologists'),
+('Krishna','f',22,'98647638', '5, Sait Clny 2nd St, Egmore','Cardiologists'),
+('Suyesh','m',23,'7860992748','20/30/a, Ganga Ind Estate, P L Lokhande Marg, Chembur','Neurologists');
+
+insert ignore into regular_schedule
+values(2,'Dr Pullen','Monday','10:00','12:00'),
+(3,'Dr Fillmore','Monday','12:00','02:00'),
+(4,'Dr Mangle','Tuesday','10:00','1:00'),
+(5,'Dr Ken Hurt','Thursday','01:00','05:00'),
+(6,'Dr B. Sick','Saturday','12:00','03:00'),
+(7,'Dr Watamaniuk','Monday','05:00','08:00'),
+(8,'Dr Lipp','Tuesday','12:00','2:00'),
+(9,'Dr Carey','Friday','03:00','05:00'),
+(10,'Dr Nervo','Wednesday','04:00','06:00'),
+(11,'Dr Hurt','Sunday','12:00','2:00');
